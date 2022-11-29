b8212c8f9346fe30239125a3abb8c24bcffaa597
Darwish98 <66258375+Darwish98@users.noreply.github.com>
2022-11-29 22:03:26 +0100

    Add EF and SqLite

---
+++ CarOrdering/CarOrderingBackend/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarOrderingBackend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CarModel> CarModels { get; set; }
+
+        //Where to store Sqlite Db
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data source= Database\CarDb.db");
+    }
+}
---
+++ CarOrdering/CarOrderingBackend/Models/CarModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarOrderingBackend.Models
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Description { get; set; }
+    }
+}
